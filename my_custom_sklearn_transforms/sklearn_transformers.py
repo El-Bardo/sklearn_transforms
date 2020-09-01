d42b7dfb20f374792bf446d29d92e9a5d9c0412c
bard <48396258+El-Bardo@users.noreply.github.com>
2020-08-31 19:01:53 -0500

    Add files via upload

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,10 @@
+import pandas as pd
+from sklearn.utils import resample
+# Nota: Todas las Clases de "SkLearn" deben tener los métodos "transform" y "fit"
+# Las Clases "BaseEstimator" y "TransformerMixin" pertenecientes a "sklearn.base" nos permitirán que
+# la Clase "DataSetBalancer" herede métodos pertenecientes a "SkLearn"
 from sklearn.base import BaseEstimator, TransformerMixin
+
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -17,11 +23,6 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
 
 
-# Nota: Todas las Clases de "SkLearn" deben tener los métodos "transform" y "fit"
-# Las Clases "BaseEstimator" y "TransformerMixin" pertenecientes a "sklearn.base" nos permitirán que
-# la Clase "DataSetBalancer" herede métodos pertenecientes a "SkLearn"
-from sklearn.base import BaseEstimator, TransformerMixin
-
 class DataSetBalancer( BaseEstimator , TransformerMixin ):
     
     def __init__( self , X ):
@@ -36,7 +37,7 @@ class DataSetBalancer( BaseEstimator , TransformerMixin ):
         minimum = min( len( self.X['PROFILE'][ self.X['PROFILE'] == 'advanced_data_science' ] ) , len( self.X['PROFILE'][ self.X['PROFILE'] == 'beginner_front_end' ] ) , len( self.X['PROFILE'][ self.X['PROFILE'] == 'beginner_data_science' ] ) , len( self.X['PROFILE'][ self.X['PROFILE'] == 'advanced_backend' ] ) , len( self.X['PROFILE'][ self.X['PROFILE'] == 'advanced_front_end' ] ) , len( self.X['PROFILE'][ self.X['PROFILE'] == 'beginner_backend' ] ) )
  
         # Igualamos la cardinalidad del conjunto muestra de cada registro
-        from sklearn.utils import resample
+        
         aux_df = pd.concat( [ resample( self.X[ self.X['PROFILE'] == 'advanced_data_science' ] , replace = True , n_samples = minimum , random_state = 100 ) , resample( self.X[ self.X['PROFILE'] == 'beginner_front_end' ] , replace = True , n_samples = minimum , random_state = 100 ) , resample( self.X[ self.X['PROFILE'] == 'beginner_data_science' ] , replace = True , n_samples = minimum , random_state = 100 ) , resample( self.X[ self.X['PROFILE'] == 'advanced_backend' ] , replace = True , n_samples = minimum , random_state = 100 ) , resample( self.X[ self.X['PROFILE'] == 'advanced_front_end' ] , replace = True , n_samples = minimum , random_state = 100 ) , resample( self.X[ self.X['PROFILE'] == 'beginner_backend' ] , replace = True , n_samples = minimum , random_state = 100 ) ] )
         
         # Devolvemos un nuevo dataframe equilibrado
